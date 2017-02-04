386bfbde35fc19a95a94a4a90b0602eb039fb36c
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-02-04 20:04:14 +0200

    prevent users from submitting the payment with a different plan id value

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -9,7 +9,12 @@ class SubscriptionsController extends Controller
     public function store(Request $request){
         $plan = Plan::findOrFail($request->plan);
 
+        if ($request->user()->subscribedToPlan($plan->braintree_plan, 'main')) {
+            return redirect('home')->with('error', 'Unauthorised operation');
+        }
+
         $request->user()->newSubscription('main', $plan->braintree_plan)->create($request->payment_method_nonce);
+
         return redirect('home');
     }
 }
